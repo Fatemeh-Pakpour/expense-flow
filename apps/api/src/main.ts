8d29a5dabdf5f4739c3525acf6271eafac831df8
Fatemeh Pakpour <49231214+Fatemeh-Pakpour@users.noreply.github.com>
2026-05-23 21:39:27 +0200

    adding exceptionFilter

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
     credentials: true
   });
 
+  app.useGlobalFilters(new GlobalExceptionFilter());
   app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT ?? 3001);
